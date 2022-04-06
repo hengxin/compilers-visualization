e993f2b6c2d172663e9f58907b7e58372bdcffca
huangmengbin <1352482583@qq.com>
2022-04-06 23:28:37 +0800

    protobuf

---
+++ gen.sh
@@ -0,0 +1,1 @@
+protoc --java_out='src/main/generated'  src/main/resources/protobuf/*.proto
---
+++ src/test/java/UserBaseServiceTest.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import hmb.protobuf.User;
+
+
+public class UserBaseServiceTest {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            URI uri = new URI("http", null, "127.0.0.1", 9988, "/getUserInfo", "", null);
+            HttpPost post = new HttpPost(uri);
+            User.UserInfoRequest.Builder builder = User.UserInfoRequest.newBuilder();
+            builder.setUserId(10086L);
+            post.setEntity(new ByteArrayEntity(builder.build().toByteArray()));
+            post.setHeader("Content-Type", "application/x-protobuf");
+            HttpResponse response = httpClient.execute(post);
+            int code = response.getStatusLine().getStatusCode();
+            System.out.println("\n\n\n\n");
+            if (code == 200) {
+                InputStream content = response.getEntity().getContent();
+                User.UserInfoResponse result = User.UserInfoResponse.parseFrom(content);
+                System.out.println("\n\n\nresult\n:" + result.toString());
+            } else {
+                System.err.println(code);
+            }
+        }
+    }
+}
