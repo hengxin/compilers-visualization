c0a21039515cb1dfd1c310982258dbde3794ba68
huangmengbin <1352482583@qq.com>
2022-05-09 12:56:01 +0800

    showAll example

--- src/components/ShowAll.vue
+++ src/components/ShowAll.vue
@@ -1,22 +1,71 @@
 <template>
-  <div>ShowAll</div>
+  <a-button @click="back">back</a-button>
+  <a-button @click="debug">debug</a-button>
+  <a-col :span="11">
+    <a-card v-for="(op, idx) in optionList" :key="idx" ref="vAtnList">
+      <v-chart
+        class="chart"
+        :option="op"
+        style="height: 300%; width: 100%"
+      ></v-chart>
+    </a-card>
+  </a-col>
 </template>
 
 <script>
-import { message } from "ant-design-vue";
-import { loaded, getInitState } from "@/data/show_all";
+import { Button, Card, Row, Col, message } from "ant-design-vue";
+import VChart from "vue-echarts";
+
+import {
+  loaded,
+  getInitState,
+  getOptionsList,
+  debug,
+  listenOptionList
+} from "@/data/show_all";
 
 export default {
   name: "ShowAll",
-  mounted() {
+  components: {
+    VChart,
+    AButton: Button,
+    ACard: Card,
+    // eslint-disable-next-line vue/no-unused-components
+    ARow: Row,
+    ACol: Col
+  },
+  beforeCreate() {
     if (!loaded()) {
       message.error("unloaded");
       this.$router.push("input");
     } else {
       console.log(getInitState());
     }
+  },
+  mounted() {
+    this.optionList = getOptionsList();
+    listenOptionList(this.optionList);
+  },
+  data() {
+    return {
+      optionList: [],
+      num: 0
+    };
+  },
+  methods: {
+    back() {
+      this.$router.push("input");
+    },
+    debug() {
+      ++this.num;
+      debug(this.num);
+    }
   }
 };
 </script>
-
-<style scoped></style>
+<style scoped>
+.chart {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/data/show_all.ts
+++ src/data/show_all.ts
@@ -1,23 +1,125 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { hmb } from "../gen/proto";
+import { message } from "ant-design-vue";
+import { input } from "../router";
 import proto = hmb.protobuf;
 
-let succeed = false;
-let myInitState: proto.IInitialState;
+const globalConstOption = {
+  title: {
+    text: "",
+    textStyle: {
+      color: "rgb(0,0,255)",
+      fontSize: 15
+    }
+  },
+  tooltip: {},
+  animationDurationUpdate: 1,
+  animationEasingUpdate: "quinticInOut",
+  series: [
+    {
+      type: "graph",
+      layout: "force",
+      symbolSize: 25,
+      animation: false,
+      draggable: true,
+      roam: true,
+      label: {
+        show: true
+      },
+      edgeSymbol: ["circle", "arrow"],
+      edgeSymbolSize: [4, 10],
+      edgeLabel: {
+        fontSize: 12
+      },
+      data: [],
+      links: [],
+      lineStyle: {
+        opacity: 0.9,
+        width: 2
+      },
+      autoCurveness: true,
+      force: {
+        repulsion: 4000,
+        edgeLength: [30, 50],
+        layoutAnimation: true,
+        // friction: 1,
+        gravity: 0.2
+      }
+    }
+  ]
+};
+
+let succeed_ = false;
+let initState_: proto.IInitialState;
+let globalOptionList_: any[] = [];
+
+function _setOptionList(list: proto.ISubAugmentedTransitionNetwork[]) {
+  globalOptionList_ = []; //必须要清空，否则会元素重复
+  for (const subATNElement of list) {
+    const option = JSON.parse(JSON.stringify(globalConstOption));
+    option.title.text = subATNElement.ruleName;
+    option.series[0].data = subATNElement.graphNode;
+    option.series[0].links = subATNElement.graphEdge;
+    globalOptionList_.push(option);
+  }
+}
 
 export default function setMainResponse(resp: proto.MainResponse): boolean {
+  if (succeed_) {
+    console.log("re init");
+  }
   if (!resp.initialState) {
     console.log("resp.initialState = " + resp.initialState);
     return false;
   }
-  myInitState = resp.initialState;
-  succeed = true;
+  initState_ = resp.initialState;
+  if (!resp.initialState.parserATN.subATN) {
+    console.log("parserATN = " + resp.initialState.parserATN.subATN);
+    return false;
+  }
+  _setOptionList(resp.initialState.parserATN.subATN);
+  succeed_ = true;
   return true;
 }
 
 export function loaded(): boolean {
-  return succeed;
+  return succeed_;
+}
+
+function check(): void {
+  if (!succeed_) {
+    message.error("unloaded!").then();
+    input().then(() => {
+      throw "";
+    });
+  }
 }
 
 export function getInitState(): proto.IInitialState {
-  return myInitState;
+  return initState_;
+}
+
+export function getOptionsList(): any[] {
+  check();
+  return globalOptionList_;
+}
+
+const colorList = [
+  "rgb(0,0,255)",
+  "rgb(0,255,0)",
+  "rgb(255,0,0)",
+  "rgb(0,255,255)",
+  "rgb(255,255,0)",
+  "rgb(255,0,255)"
+];
+
+export function listenOptionList(optionList: any[]): void {
+  globalOptionList_ = optionList;
+}
+
+export function debug(num: number): void {
+  for (const op of globalOptionList_) {
+    op.series[0].data[0].itemStyle.color = colorList[num % colorList.length];
+  }
 }
