9071fb6d138ebc1073748e0e272d44a85a2a1668
huangmengbin <1352482583@qq.com>
2022-05-13 21:46:44 +0800

    feat: 暂存textInput

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -68,116 +68,57 @@ export default {
     ATextarea: Textarea
   },
   data() {
-    return {
-      grammarName: "Cmm",
-      lexer:
-        "lexer grammar CmmLexer;\n" +
-        "\n" +
-        "\n" +
-        "Whitespace\n" +
-        "    :   [ \\t]+\n" +
-        "        -> channel(HIDDEN)\n" +
-        "    ;" +
-        "\n" +
-        "Newline\n" +
-        "    :   (   '\\r' '\\n'?\n" +
-        "        |   '\\n'\n" +
-        "        )\n" +
-        "        -> channel(HIDDEN)\n" +
-        "    ;\n" +
-        "\n" +
-        "\n" +
-        "\n" +
-        "TYPE : 'int' | 'float';\n" +
-        "\n" +
-        "LP : '(';\n" +
-        "RP : ')';\n" +
-        "LB : '[';\n" +
-        "RB : ']';\n" +
-        "LC : '{';\n" +
-        "RC : '}';\n" +
-        "\n" +
-        "RELOP : '<' | '<=' | '>' | '>=' | '==' | '!=';\n" +
-        "\n" +
-        "PLUS : '+';\n" +
-        "MINUS : '-';\n" +
-        "STAR : '*';\n" +
-        "DIV : '/';\n" +
-        "\n" +
-        "\n" +
-        "SEMI : ';';\n" +
-        "COMMA : ',';\n" +
-        "\n" +
-        "ASSIGNOP : '=';\n" +
-        "\n" +
-        "DOT : '.';\n" +
-        "\n" +
-        "\n" +
-        "\n" +
-        "ID : Nondigit (Nondigit | Digit)*;\n" +
-        "\n" +
-        "INT : NonZeroDigit Digit*;\n" +
-        "\n" +
-        "FLOAT : Science | Real;\n" +
-        "\n" +
-        "\n" +
-        "fragment\n" +
-        "Nondigit\n" +
-        "    :   [a-zA-Z_]\n" +
-        "    ;\n" +
-        "\n" +
-        "fragment\n" +
-        "Digit\n" +
-        "    :   [0-9]\n" +
-        "    ;\n" +
-        "\n" +
-        "fragment\n" +
-        "NonZeroDigit\n" +
-        "    :   [1-9]\n" +
-        "    ;\n" +
-        "\n" +
-        "\n" +
-        "fragment\n" +
-        "Real\n" +
-        "    :   Digit+ DOT Digit+\n" +
-        "    ;\n" +
-        "\n" +
-        "fragment\n" +
-        "Science\n" +
-        "    :   ((Digit* DOT Digit+)|(Digit+ DOT Digit*)) ('e'|'E') (('+'|'-')?) Digit+\n" +
-        "    ;",
-      parser:
-        "parser grammar CmmParser;\n" +
-        "\n" +
-        "options {\n" +
-        "  tokenVocab=CmmLexer;\n" +
-        "}\n" +
-        "\n" +
-        "program: stmt+ ;\n" +
-        "\n" +
-        "stmt:\n" +
-        "    expr1 SEMI |\n" +
-        "    expr2 SEMI |\n" +
-        "    expr3 SEMI ;\n" +
-        "\n" +
-        "expr1:\n" +
-        "    INT |\n" +
-        "    LP expr1 RP;\n" +
-        "\n" +
-        "expr2:\n" +
-        "    FLOAT |\n" +
-        "    LP expr2 RP;\n" +
-        "\n" +
-        "expr3:\n" +
-        "    LP* ID RP* ;\n",
-      code: "((2));\n" + "(((3.3)));"
-    };
+    return {};
   },
   computed: {
-    ...mapState(["userId"])
+    ...mapState([
+      "userId",
+      "globalGrammarName",
+      "globalLexerString",
+      "globalParserString",
+      "globalCodeString"
+    ]),
+    grammarName: {
+      get() {
+        return this.globalGrammarName;
+      },
+      set(val) {
+        this.setGrammarString(val);
+      }
+    },
+    lexer: {
+      get() {
+        return this.globalLexerString;
+      },
+      set(val) {
+        this.setLexer(val);
+      }
+    },
+    parser: {
+      get() {
+        return this.globalParserString;
+      },
+      set(val) {
+        this.setParser(val);
+      }
+    },
+    code: {
+      get() {
+        return this.globalCodeString;
+      },
+      set(val) {
+        this.setCode(val);
+      }
+    }
   },
   methods: {
-    ...mapMutations(["setUserId"]),
+    ...mapMutations([
+      "setUserId",
+      "setGrammarString",
+      "setLexer",
+      "setParser",
+      "setCode"
+    ]),
     ok() {
       this.setUserId(0);
       const userId = this.userId;
--- src/store.js
+++ src/store.js
@@ -3,12 +3,126 @@ import { createStore } from "vuex";
 const myStore = createStore({
   state() {
     return {
-      userId: 0
+      userId: 0,
+      globalGrammarName: "Cmm",
+      globalLexerString:
+        "lexer grammar CmmLexer;\n" +
+        "\n" +
+        "\n" +
+        "Whitespace\n" +
+        "    :   [ \\t]+\n" +
+        "        -> channel(HIDDEN)\n" +
+        "    ;" +
+        "\n" +
+        "Newline\n" +
+        "    :   (   '\\r' '\\n'?\n" +
+        "        |   '\\n'\n" +
+        "        )\n" +
+        "        -> channel(HIDDEN)\n" +
+        "    ;\n" +
+        "\n" +
+        "\n" +
+        "\n" +
+        "TYPE : 'int' | 'float';\n" +
+        "\n" +
+        "LP : '(';\n" +
+        "RP : ')';\n" +
+        "LB : '[';\n" +
+        "RB : ']';\n" +
+        "LC : '{';\n" +
+        "RC : '}';\n" +
+        "\n" +
+        "RELOP : '<' | '<=' | '>' | '>=' | '==' | '!=';\n" +
+        "\n" +
+        "PLUS : '+';\n" +
+        "MINUS : '-';\n" +
+        "STAR : '*';\n" +
+        "DIV : '/';\n" +
+        "\n" +
+        "\n" +
+        "SEMI : ';';\n" +
+        "COMMA : ',';\n" +
+        "\n" +
+        "ASSIGNOP : '=';\n" +
+        "\n" +
+        "DOT : '.';\n" +
+        "\n" +
+        "\n" +
+        "\n" +
+        "ID : Nondigit (Nondigit | Digit)*;\n" +
+        "\n" +
+        "INT : NonZeroDigit Digit*;\n" +
+        "\n" +
+        "FLOAT : Science | Real;\n" +
+        "\n" +
+        "\n" +
+        "fragment\n" +
+        "Nondigit\n" +
+        "    :   [a-zA-Z_]\n" +
+        "    ;\n" +
+        "\n" +
+        "fragment\n" +
+        "Digit\n" +
+        "    :   [0-9]\n" +
+        "    ;\n" +
+        "\n" +
+        "fragment\n" +
+        "NonZeroDigit\n" +
+        "    :   [1-9]\n" +
+        "    ;\n" +
+        "\n" +
+        "\n" +
+        "fragment\n" +
+        "Real\n" +
+        "    :   Digit+ DOT Digit+\n" +
+        "    ;\n" +
+        "\n" +
+        "fragment\n" +
+        "Science\n" +
+        "    :   ((Digit* DOT Digit+)|(Digit+ DOT Digit*)) ('e'|'E') (('+'|'-')?) Digit+\n" +
+        "    ;",
+      globalParserString:
+        "parser grammar CmmParser;\n" +
+        "\n" +
+        "options {\n" +
+        "  tokenVocab=CmmLexer;\n" +
+        "}\n" +
+        "\n" +
+        "program: stmt+ ;\n" +
+        "\n" +
+        "stmt:\n" +
+        "    expr1 SEMI |\n" +
+        "    expr2 SEMI |\n" +
+        "    expr3 SEMI ;\n" +
+        "\n" +
+        "expr1:\n" +
+        "    INT |\n" +
+        "    LP expr1 RP;\n" +
+        "\n" +
+        "expr2:\n" +
+        "    FLOAT |\n" +
+        "    LP expr2 RP;\n" +
+        "\n" +
+        "expr3:\n" +
+        "    LP* ID RP* ;\n",
+      globalCodeString: "((2));\n" + "(((3.3)));"
     };
   },
   mutations: {
     setUserId(state, userId) {
       state.userId = userId;
+    },
+    setGrammarString(state, grammarName) {
+      state.globalGrammarName = grammarName;
+    },
+    setLexer(state, lexer) {
+      state.globalLexerString = lexer;
+    },
+    setParser(state, parser) {
+      state.globalParserString = parser;
+    },
+    setCode(state, code) {
+      state.globalCodeString = code;
     }
   }
 });
