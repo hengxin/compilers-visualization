945c028171259e6b7a7f42fbbde5cb1625963e05
huangmengbin <1352482583@qq.com>
2022-06-02 01:18:26 +0800

    fix: 修复启动命令

--- compilers_visualization_backend/gen.sh
+++ compilers_visualization_backend/gen.sh
@@ -1,1 +1,1 @@
-protoc --java_out='src/main/generated'  src/main/resources/protobuf/*.proto
+mkdir -p src/main/generated && protoc --java_out='src/main/generated'  src/main/resources/protobuf/*.proto
